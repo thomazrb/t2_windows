d33db340ebfaec4b3e9fcca7c59f69ceefeb3ec6
Thomaz Botelho <thomazrb@gmail.com>
2024-08-04 20:25:18 -0300

    Hello World Beeware

---
+++ helloworld/src/helloworld/app.py
@@ -0,0 +1,26 @@
+import toga
+from toga.style import Pack
+from toga.style.pack import COLUMN, ROW
+
+
+class HelloWorld(toga.App):
+    def startup(self):
+        main_box = toga.Box(style=Pack(direction=COLUMN))
+        name_label = toga.Label("Your name:", style=Pack(padding=(0,5)))
+        self.name_input = toga.TextInput(style=Pack(flex=1))
+        name_box = toga.Box(style=Pack(direction=ROW, padding=5))
+        button = toga.Button('Say Hello!', on_press=self.say_hello, style=Pack(padding=5))
+        name_box.add(name_label)
+        name_box.add(self.name_input)
+        main_box.add(name_box)
+        main_box.add(button)
+        self.main_window = toga.MainWindow(title=self.formal_name)
+        self.main_window.content = main_box
+        self.main_window.show()
+
+    def say_hello(self, widget):
+        self.main_window.info_dialog(f'Hello, {self.name_input.value}', "Hi there!")
+
+
+def main():
+    return HelloWorld()
